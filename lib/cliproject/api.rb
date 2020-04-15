381b5072938bf4c4da8ff2cc0013b1836da410ec
Kevin Mathias <komathias1@gmail.com>
2020-04-15 09:01:11 +0000

    200am_04/15/2020

--- lib/cliproject/api.rb
+++ lib/cliproject/api.rb
@@ -5,7 +5,8 @@ class Api
     url ="http://dnd5eapi.co/api/classes/#{class_selection}/"
     # response = Net::HTTP.get(URI(url)) #class example: didnt work
     r = HTTParty.get(url) #google example, did work. Thanks google!
-    puts r
+    classes = r.parsed_response
+    puts classes
   end
   
 end
